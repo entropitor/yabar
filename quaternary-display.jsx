f8bce9db15e71df69f9947bed1cfd3ea9b377a7b
Jens Claes <896093+entropitor@users.noreply.github.com>
2024-04-18 09:57:23 +0200

    add quaternary display

---
+++ quaternary-display.jsx
@@ -0,0 +1,10 @@
+import Left from "./lib/Left.jsx";
+
+export const refreshFrequency = false;
+export const command = "./yabar/scripts/left.sh 3";
+
+export const render = ({ output }) => {
+  return <Left output={output} />;
+};
+
+export default null;
